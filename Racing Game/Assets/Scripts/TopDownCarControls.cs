03d234a8ebb9e70cde6504b4369bd260836ad20d
husamfares <152183374+husamfares@users.noreply.github.com>
2023-12-04 20:26:54 +0300

    part 2 of changes

--- Racing Game/Assets/Scripts/TopDownCarControls.cs
+++ Racing Game/Assets/Scripts/TopDownCarControls.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class TopDownCarControls : MonoBehaviour
 {
+     
     [Header("car settings")]
+    public float driftFactor = 0.90f;
     public float accelerationFactor = 30.0f;
     public float turnFactor = 3.5f;
+    public float maxSpeed = 20;
 
         //local variables 
 
     float accelerationInput=0;
     float steeringInput = 0;
     float rotationAngle = 0;
-
+    float velocityUp = 0;
     //Components
 
     Rigidbody2D carRigidbody2d;//to have access the rigidbody component in unity
@@ -40,31 +43,68 @@ public class TopDownCarControls : MonoBehaviour
      void FixedUpdate()
     {
         ApplyEnginForce();
+        KillOrthogonalVelocity();
         ApplySteering();
 
     }
 
     void ApplyEnginForce()
     {
+        velocityUp = Vector2.Dot(transform.up , carRigidbody2d.velocity);
+
+        if (velocityUp > maxSpeed && accelerationInput > 0)
+        {
+            return;
+        }
+        //When you "reverse", the speed will slow down
+        if (velocityUp < -maxSpeed * 0.5f && accelerationInput < 0) 
+        {
+            return;
+        }
+
+        if(carRigidbody2d.velocity.sqrMagnitude > maxSpeed * maxSpeed && accelerationInput > 0)
+        {
+            return;
+        }
+        //It slows down the car if the condition is true
+        if (accelerationInput == 0)
+            carRigidbody2d.drag = Mathf.Lerp(carRigidbody2d.drag, 3.0f, Time.fixedDeltaTime * 3);
+        else
+            carRigidbody2d.drag = 0;
+       
         //Create a force for the engine(the vector will be pplying as the force)
         Vector2 engineForceVector = transform.up * accelerationInput * accelerationFactor;
 
-                                                                                                 //engineForceVector: this is the variable inwhich the force aplyed oon the car object will be stord in 
+                       //engineForceVector: this is the variable inwhich the force aplyed oon the car object will be stord in 
 
         //apply force that will be pushing the car forward
         carRigidbody2d.AddForce(engineForceVector,ForceMode2D.Force);
-                                                                                                  //carRigidbody2d:add this varible to the rigidbody's component to apply the force onn the car object
-                                                                                                 //ForceMode2D.Force indicates that the force is applied continuously and scaled by the Rigidbody's mass.
+                                                                                             
     
     }
 
     void ApplySteering()
     {
-        //update the rotation angle based  on input
-        rotationAngle -= steeringInput * turnFactor;
-        //apply steering the rotating the car object
-        carRigidbody2d.MoveRotation(rotationAngle);
+        //Preventing the car from moving before it moves
+        float minSpeedBeforeAllowTurningFactor = (carRigidbody2d.velocity.magnitude/8);
 
+        //Clamp01(minSpeedBeforeAllowTurningFactor) : A period from 0 to 1. If the value is 0, it is multiplied by the values,
+        //so it does not move to the right or to the left because the car is parked.
+        minSpeedBeforeAllowTurningFactor = Mathf.Clamp01(minSpeedBeforeAllowTurningFactor);
+
+        rotationAngle -= steeringInput * turnFactor * minSpeedBeforeAllowTurningFactor;
+        
+        carRigidbody2d.MoveRotation(rotationAngle);
+    }
+    void KillOrthogonalVelocity()
+    {   // Here I want to calculate the forwardVelocity of the car
+        Vector2 forwardVelocity = transform.up * Vector2.Dot(carRigidbody2d.velocity, transform.up);
+
+       
+        Vector2 rightVelocity = transform.right * Vector2.Dot(carRigidbody2d.velocity, transform.right);
+
+        //  We fixed the driftFactor to a constant value and multiplied it by the forwardVelocity to reduce the drift
+        carRigidbody2d.velocity = forwardVelocity + rightVelocity * driftFactor;
     }
 
     //s function to set the input for the vectors which determines the force affevting the car
